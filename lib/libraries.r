2dc63c3ebd6149bd37301d754895b245ea222bbc
MatevzRaspet <matevz.raspet@gmail.com>
2018-10-03 19:05:37 +0200

    dodana knjižnica stringr

--- lib/libraries.r
+++ lib/libraries.r
@@ -6,7 +6,7 @@ library(gsubfn)
 library(ggplot2)
 library(reshape2)
 library(shiny)
-
+library(stringr)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
