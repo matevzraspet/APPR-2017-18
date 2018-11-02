a581808f597f82282681b38dad3804e7c40377c8
matevzraspet <matevz.raspet@gmail.com>
2018-11-02 10:51:31 +0100

    Update vizualizacija.r

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -187,3 +187,44 @@ placa_2017 <- ggplot() + geom_polygon(data = placa %>% filter(leto == 2017) %>%
   ggtitle("Povprečna plača v letu 2010 po občinah")
 placa_2017
 
+
+
+#############################
+#Grupiranje, izračun igralnih pozicij s pomočjo grupiranja
+### Hotel sem pogledati, če lahko iz danih golov in asistenc igralca izluščimo igralno pozicijo oziroma tip pozicije (napad,sredina,obramba)
+
+
+set.seed(20)
+pozicije <- kmeans(igralci[,c(6,8)], 3, nstart = 20)
+pozicije
+
+pozicije$cluster <- as.factor(pozicije$cluster)
+ggplot(igralci, aes(asistence, zadeti.goli, color = pozicije$cluster)) +
+  ggtitle("Izračun igralnih pozicij s pomočjo grupiranja") + xlab("Asistence") + ylab("Zadeti goli") +
+  geom_point() + 
+  scale_fill_manual(name="Tip igralne pozicije", values=c("red","green","blue", labels=c("obramba", "sredina", "napad")))
+
+### Opomba : kot napadalne pozicije interpretiramo: Centre forward, Second striker;
+# kot sredina : attacking midfield, central midfield, left winger, right winger;
+# kot obramba : centre back, defensive midfield, goalkeeper, right back, left back
+
+### Dejanske pozicije glede na statistiko
+
+ggplot(igralci, aes(asistence, zadeti.goli, color = pozicija)) +
+  ggtitle("Igralne pozicije") + xlab("Asistence") + ylab("Zadeti goli") +
+  geom_point() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
